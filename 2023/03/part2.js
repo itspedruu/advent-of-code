f596f2391e593e9845423e7e41d032b29df0f3e7
Pedro Pinto <pedropinto.01@hotmail.com>
2023-12-03 12:23:34 +0000

    day 03

---
+++ 2023/03/part2.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+const lines = fs.readFileSync('./input.txt', { encoding: 'utf8'}).split('\r\n');
+
+let N = lines.length, M = lines[0].length;
+
+// hash -> (number, adjacents)
+const numbers = {};
+
+let numberStr = '';
+let adjacents = [];
+
+for (let i = 0; i < lines.length; i++) {
+  for (let j = 0; j < lines[i].length; j++) {
+    let charCode = lines[i].charCodeAt(j);
+
+    if (charCode >= 48 && charCode <= 57) {
+      numberStr += lines[i][j];
+      adjacents.push(`${i}.${j}`);
+    } else {
+      const number = Number.parseInt(numberStr);
+
+      for (const hash of adjacents) {
+        numbers[hash] = [number, adjacents];
+      }
+
+      numberStr = '';
+      adjacents = [];
+    }
+  }
+}
+
+function getValidNeighbourHashes(line, column) {
+ return [
+    [line - 1, column - 1],
+    [line - 1, column],
+    [line - 1, column + 1],
+    [line, column - 1],
+    [line, column + 1],
+    [line + 1, column - 1],
+    [line + 1, column],
+    [line + 1, column + 1]
+  ].filter(([n, m]) => n >= 0 && n < N && m >= 0 && m < M)
+   .map(([n, m]) => `${n}.${m}`);
+}
+
+let sum = 0;
+
+for (let i = 0; i < lines.length; i++) {
+  for (let j = 0; j < lines[i].length; j++) {
+    if (lines[i][j] !== '*') {
+      continue;
+    }
+
+    const neighbours = getValidNeighbourHashes(i, j);
+
+    let currentNumbers = [];
+    let visited = [];
+
+    for (const hash of neighbours) {
+      if (hash in numbers && !visited.includes(hash)) {
+        currentNumbers.push(numbers[hash][0]);
+        visited.push(...numbers[hash][1]);
+      }
+    }
+
+    if (currentNumbers.length === 2) {
+      sum += currentNumbers[0] * currentNumbers[1];
+    }
+  }
+}
+
+console.log(`Sum: ${sum}`);
