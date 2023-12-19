a15898584921f288b50dbf15dd5c491f0f1fe100
Pedro Pinto <pedropinto.01@hotmail.com>
2023-12-19 23:29:14 +0000

    optimized day 17

--- 2023/17/part2.js
+++ 2023/17/part2.js
@@ -1,4 +1,3 @@
-// todo: optimize with different data structure for queue
 const fs = require('fs');
 const grid = fs.readFileSync('./input.txt', 'utf8')
   .split('\r\n')
@@ -9,38 +8,85 @@ const WIDTH = grid[0].length;
 const HEIGHT = grid.length;
 
 // (coord, dir, numSteps)
-const queue = [[[0, 0], [0, 1], 0], [[0, 0], [1, 0], 0]];
+const heap = [[[0, 0], [0, 1], 0], [[0, 0], [1, 0], 0]];
 const dist = {
   '0.0.0.1.0': 0,
   '0.0.1.0.0': 0
 };
 
-function minIndex() {
-  let index = 0;
-  let minDist = Infinity;
+const getLeftIndex = (index) => index * 2 + 1;
+const getRightIndex = (index) => index * 2 + 2;
+const getParentIndex = (index) => Math.floor((index - 1) / 2);
 
-  for (let i = 0; i < queue.length; i++) {
-    const hash = [queue[i][0].join('.'), queue[i][1].join('.'), queue[i][2]].join('.');
-
-    if (dist[hash] < minDist) {
-      minDist = dist[hash];
-      index = i;
-    }
+function compare(i, j) {
+  if (i < 0 || i >= heap.length || j < 0 || j >= heap.length) {
+    return false;
   }
 
-  return index;
+  const [[xI, yI], [dxI, dyI], numStepsI] = heap[i];
+  const hashI = `${xI}.${yI}.${dxI}.${dyI}.${numStepsI}`;
+
+  const [[xJ, yJ], [dxJ, dyJ], numStepsJ] = heap[j];
+  const hashJ = `${xJ}.${yJ}.${dxJ}.${dyJ}.${numStepsJ}`;
+
+  return dist[hashI] < dist[hashJ];
 }
 
-const pai = {};
+function swap(i, j) {
+  const temp = heap[i];
 
-while (queue.length) {
-  const index = minIndex();
-  const [[x, y], [dx, dy], numSteps] = queue.splice(index, 1)[0];
+  heap[i] = heap[j];
+  heap[j] = temp;
+}
+
+function extractMin() {
+  const min = heap[0];
+
+  heap[0] = heap[heap.length - 1];
+  heap.pop();
+
+  let index = 0;
+
+  while (getLeftIndex(index) < heap.length) {
+    let smallerIndex = getLeftIndex(index);
+
+    if (compare(getRightIndex(index), smallerIndex)) {
+      smallerIndex = getRightIndex(index);
+    }
+
+    if (compare(index, smallerIndex)) {
+      break;
+    }
+
+    swap(index, smallerIndex);
+    index = smallerIndex;
+  }
+
+  return min;
+}
+
+function addValue(value) {
+  heap.push(JSON.parse(JSON.stringify(value)));
+  
+  let index = heap.length - 1;
+
+  while (compare(index, getParentIndex(index))) {
+    swap(getParentIndex(index), index);
+    index = getParentIndex(index);
+  }
+}
+
+while (heap.length) {
+  const [[x, y], [dx, dy], numSteps] = extractMin();
   const hash = `${x}.${y}.${dx}.${dy}.${numSteps}`;
 
+  if (hash.startsWith(`${HEIGHT - 1}.${WIDTH - 1}`)) {
+    continue;
+  }
+
   const neighboursDirections = numSteps >= 4
-      ? [[dx, dy], [dy, dx], [-dy, -dx]]
-      : [[dx, dy]];
+    ? [[dx, dy], [dy, dx], [-dy, -dx]]
+    : [[dx, dy]];
 
   for (let i = 0; i < neighboursDirections.length; i++) {
     const [ndx, ndy] = neighboursDirections[i];
@@ -54,13 +100,12 @@ while (queue.length) {
     const nHash = `${nx}.${ny}.${ndx}.${ndy}.${nextNumSteps}`;
 
     if (!(nHash in dist)) {
-      queue.push([[nx, ny], [ndx, ndy], nextNumSteps]);
+      addValue([[nx, ny], [ndx, ndy], nextNumSteps]);
       dist[nHash] = Infinity;
     }
 
     if (dist[hash] + grid[nx][ny] < dist[nHash]) {
       dist[nHash] = dist[hash] + grid[nx][ny]; 
-      pai[nHash] = hash;
     }
   }
 }
